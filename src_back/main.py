a8f18f931deedaea2403519503d15001a65a59ff
komaroff <komaroff.sergei@gmail.com>
2025-10-14 18:00:17 +0300

    update

--- src_back/main.py
+++ src_back/main.py
@@ -6,4 +6,6 @@ from server.app import create_app
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
     app = create_app()
-    web.run_app(app, host="0.0.0.0", port=8080)
+    import os
+    port = int(os.getenv("PORT", "8000"))
+    web.run_app(app, host="0.0.0.0", port=port)
