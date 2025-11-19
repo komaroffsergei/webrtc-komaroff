584e63b9f73b24f56c4dab235641a3be15ca8433
komaroff <komaroff.sergei@gmail.com>
2025-11-19 14:15:13 +0300

    fix for json encode

--- src_whisper_ruby/whisper_ruby/logger.rb
+++ src_whisper_ruby/whisper_ruby/logger.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "time"
+require "json"
 
 module WhisperRuby
   class Logger
@@ -10,11 +11,12 @@ module WhisperRuby
     end
 
     def log(message, type: "info", service: STACK_SERVICE_NAME, name: "")
+      safe_message = message.to_s.encode("UTF-8", invalid: :replace, undef: :replace)
       payload = {
         time: Time.now.utc.iso8601(3),
         service: service,
         type: type,
-        message: message,
+        message: safe_message,
         name: name
       }
       @nc.publish(@subject, payload.to_json)
