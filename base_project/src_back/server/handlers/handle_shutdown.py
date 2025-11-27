bf6d0890a59ee85076e203a2429a67431d882d35
komaroff <komaroff.sergei@gmail.com>
2025-11-27 18:37:58 +0300

    cleanup

--- base_project/src_back/server/handlers/handle_shutdown.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-
-async def handle_shutdown(app):
-    pcs = list(app["pcs"])
-    await asyncio.gather(*(pc.close() for pc in pcs), return_exceptions=True)
-    app["pcs"].clear()
